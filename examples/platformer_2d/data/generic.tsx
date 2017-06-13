aa9c58a95f79ab99923711565e45c0ca76d99798
sven1977 <svenmika1977@gmail.com>
2017-06-13 10:54:25 +0200

    - added data and images folders to platformer_2d

---
+++ examples/platformer_2d/data/generic.tsx
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="generic" tilewidth="16" tileheight="16" tilecount="1600" columns="40">
+ <image source="../images/generic.png" width="640" height="640"/>
+ <tile id="0">
+  <properties>
+   <property name="no_collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="offset" type="int" value="1"/>
+   <property name="slope" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="offset" type="int" value="1"/>
+   <property name="slope" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="offset" type="int" value="2"/>
+   <property name="slope" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="offset" type="int" value="1"/>
+   <property name="slope" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="offset" type="int" value="2"/>
+   <property name="slope" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="offset" type="int" value="3"/>
+   <property name="slope" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="offset" type="int" value="1"/>
+   <property name="slope" type="int" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="offset" type="int" value="2"/>
+   <property name="slope" type="int" value="-2"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="offset" type="int" value="1"/>
+   <property name="slope" type="int" value="-2"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="offset" type="int" value="3"/>
+   <property name="slope" type="int" value="-3"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="offset" type="int" value="2"/>
+   <property name="slope" type="int" value="-3"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="offset" type="int" value="1"/>
+   <property name="slope" type="int" value="-3"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="exit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="exit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="goal_state" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="liquid" value="quicksand"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ examples/platformer_2d/data/wrbc.tsx
@@ -0,0 +1,260 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wrbc" tilewidth="16" tileheight="16" tilecount="1702" columns="37">
+ <image source="../images/wrbc.png" width="600" height="750"/>
+ <tile id="0">
+  <properties>
+   <property name="no_collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="test" type="float" value="5.555"/>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="197">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="214">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="234">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="251">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="271">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="288">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="308">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="325">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="345">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="382">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="419">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="456">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="521">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="522">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="558">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="559">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="595">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="596">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="619">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="620">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="632">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="633">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="656">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="657">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="669">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="670">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="693">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="694">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="706">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="707">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="730">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="731">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="743">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="744">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="767">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="768">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="780">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="781">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="804">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="805">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="841">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="842">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="878">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+ <tile id="879">
+  <properties>
+   <property name="type" value="ladder"/>
+  </properties>
+ </tile>
+</tileset>
